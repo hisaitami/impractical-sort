cebd626b940a50fd6d2d5c90c367b09968af5735
hisaitami <hisaitami@gmail.com>
2022-12-01 11:25:53 +0900

    Stop println and return sorted result in vector

--- src/impractical_sort/sleepsort.clj
+++ src/impractical_sort/sleepsort.clj
@@ -1,13 +1,16 @@
 (ns impractical-sort.sleepsort)
 
 (defn sleepsort [coll]
-  (letfn [(f [n]
-            (Thread/sleep (* 10 n))
-            (print n))]
-    (pmap f coll)))
+  (let [r (atom [])
+        size (count coll)
+        f (fn [n]
+            (Thread/sleep (* n 10))
+            (swap! r conj n))]
+    (->> (pmap f coll)
+         (filter #(= size (count %)))
+         first)))
 
 (time (sleepsort [9 6 0 7 1 5 2 3 8 4]))
 
-;; 0"Elapsed time: 0.62599 msecs"
-;; (nil nil nil nil nil nil nil nil nil nil)
-;; 123456789 <- FIXME: 0 is shown above line
+;; "Elapsed time: 96.301592 msecs"
+;; [0 1 2 3 4 5 6 7 8 9]
