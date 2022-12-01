73aea68f2dd84500622b1d4a22b4c8fd5e5692d8
hisaitami <hisaitami@gmail.com>
2022-12-01 10:49:09 +0900

    Remove lines generated automatically

--- test/impractical_sort/core_test.clj
+++ test/impractical_sort/core_test.clj
@@ -1,7 +1,3 @@
 (ns impractical-sort.core-test
   (:require [clojure.test :refer :all]
             [impractical-sort.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
