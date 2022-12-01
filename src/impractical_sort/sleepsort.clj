538a4069398d0d1a4e6c3bae54e8193865a99b1c
hisaitami <hisaitami@gmail.com>
2022-12-01 11:14:36 +0900

    Tweak sleep time for println

--- src/impractical_sort/sleepsort.clj
+++ src/impractical_sort/sleepsort.clj
@@ -2,12 +2,12 @@
 
 (defn sleepsort [coll]
   (letfn [(f [n]
-            (Thread/sleep n)
+            (Thread/sleep (* 10 n))
             (print n))]
     (pmap f coll)))
 
 (time (sleepsort [9 6 0 7 1 5 2 3 8 4]))
 
-;; "Elapsed time: 8.782958 msecs"
-;; (nil nil nil nil nil nil nil nil nil nil)0123546798 <- FIXME
-
+;; 0"Elapsed time: 0.62599 msecs"
+;; (nil nil nil nil nil nil nil nil nil nil)
+;; 123456789 <- FIXME: 0 is shown above line
