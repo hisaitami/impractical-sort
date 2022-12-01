b0210abd7c1891c519f02d9502dcc81c5ec845bb
hisaitami <hisaitami@gmail.com>
2022-12-01 10:50:52 +0900

    Comment out test run

--- src/impractical_sort/bogosort.clj
+++ src/impractical_sort/bogosort.clj
@@ -9,6 +9,7 @@
   (if (ascending? coll) coll
       (recur (shuffle coll))))
 
-(time (bogosort [9 6 0 7 1 5 2 3 8 4]))
-;=> "Elapsed time: 6937.974985 msecs"
-;=> [0 1 2 3 4 5 6 7 8 9]
+;;(time (bogosort [9 6 0 7 1 5 2 3 8 4]))
+
+;;=> "Elapsed time: 6937.974985 msecs"
+;;=> [0 1 2 3 4 5 6 7 8 9]
