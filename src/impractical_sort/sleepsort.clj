946ffede5768fe1dde329ea0ad7d2d3b425453aa
hisaitami <hisaitami@gmail.com>
2022-12-01 11:39:10 +0900

    Add test for sleepsort module

--- src/impractical_sort/sleepsort.clj
+++ src/impractical_sort/sleepsort.clj
@@ -10,7 +10,7 @@
          (filter #(= size (count %)))
          first)))
 
-(time (sleepsort [9 6 0 7 1 5 2 3 8 4]))
+;;(time (sleepsort [9 6 0 7 1 5 2 3 8 4]))
 
 ;; "Elapsed time: 96.301592 msecs"
 ;; [0 1 2 3 4 5 6 7 8 9]
---
+++ test/impractical_sort/sleepsort_test.clj
@@ -0,0 +1,9 @@
+(ns impractical-sort.sleepsort-test
+  (:require [clojure.test :refer :all]
+            [impractical-sort.sleepsort :refer [sleepsort]]))
+
+(deftest sleepsort-test
+  (let [coll [9 6 0 7 1 5 2 3 8 4]]
+    (is (= (sort coll) (sleepsort coll))))
+  (let [coll [4 3 1 3 39 3 4 3 9 6 0 7 1 5 2 3 8 4]]
+    (is (= (sort coll) (sleepsort coll)))))
