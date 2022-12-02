2cb4f6f45da6e1acf0f2e013f523ee75110b16fa
hisaitami <hisaitami@gmail.com>
2022-12-02 14:48:04 +0900

    Update description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject impractical-sort "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Impractical sorting algorithms"
+  :url "https://github.com/hisaitami/impractical-sort"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
