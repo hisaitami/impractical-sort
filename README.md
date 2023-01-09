f7ea9d3ce7019056301e1c41e64a82285f972ed1
hisaitami <hisaitami@gmail.com>
2023-01-09 11:41:57 +0900

    Allow pred function to be specified

--- README.md
+++ README.md
@@ -13,15 +13,24 @@ Impractical sorting algorithms
 ### bogosort
 
 ```
-(use 'impractical-sort.bogosort)
+=> (use 'impractical-sort.bogosort)
 
-(time (bogosort [2 0 1 2]))
-;; "Elapsed time: 0.10525 msecs"
-;; [0 1 2 2]
+;; It takes a time
+=> (time (bogosort [9 6 0 7 1 5 2 3 8 4]))
+"Elapsed time: 1455.961248 msecs"
+[0 1 2 3 4 5 6 7 8 9]
 
-(time (bogosort [9 6 0 7 1 5 2 3 8 4]))
-;; "Elapsed time: 1455.961248 msecs"
-;; [0 1 2 3 4 5 6 7 8 9]
+;; Specify pred function explicitly
+=> (bogosort ascending? [9 6 0 7 1 5 2 3 8 4])
+[0 1 2 3 4 5 6 7 8 9]
+
+;; Reverse sorting
+=> (bogosort descending? [9 6 0 7 1 5 2 3 8 4])
+[9 8 7 6 5 4 3 2 1 0]
+
+;; Generate any bogosorted sequence
+=> (bogosort #(ascending? (take 10 %)) [11 0 5 18 17 19 14 9 1 7 10 13 15 3 8 4 12 16 6 2])
+[0 1 2 5 9 10 11 14 15 16 19 6 12 17 8 7 3 18 13 4]
 ```
 
 ### sleepsort
--- src/impractical_sort/bogosort.clj
+++ src/impractical_sort/bogosort.clj
@@ -1,25 +1,20 @@
 (ns impractical-sort.bogosort)
 
-;; 1st implementation
-(defn ascending?-take1 [[x y & _ :as coll]]
-  (if (nil? y) true
-      (if (> x y) false
-          (recur (rest coll)))))
-
-;; 2nd implementation
-(defn ascending?-take2 [coll]
-  (->> (partition-all 2 1 coll)
-       (every? #(apply <= %))))
-
-;; maybe final implementation
 (defn ascending? [coll]
   (apply <= coll))
 
-(defn bogosort [coll]
-  (if (ascending? coll) coll
-      (recur (shuffle coll))))
+(defn descending? [coll]
+  (apply >= coll))
+
+(defn bogosort
+  "Returns a sorted sequence of the items in coll.
+  However keep shuffling until a given coll is sorted."
+  ([coll]
+   (bogosort ascending? coll))
+  ([pred coll]
+   (if (pred coll) coll
+       (recur pred (shuffle coll)))))
 
 ;;(time (bogosort [9 6 0 7 1 5 2 3 8 4]))
-
-;;=> "Elapsed time: 6937.974985 msecs"
-;;=> [0 1 2 3 4 5 6 7 8 9]
+;;"Elapsed time: 6937.974985 msecs"
+;;[0 1 2 3 4 5 6 7 8 9]
