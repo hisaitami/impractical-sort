a03d0dc8532043951f4da3e860f3d97d2d943005
hisaitami <hisaitami@gmail.com>
2023-01-11 11:00:11 +0900

    Add descending?-test and shallow nesting

--- test/impractical_sort/bogosort_test.clj
+++ test/impractical_sort/bogosort_test.clj
@@ -2,16 +2,25 @@
   (:require [clojure.test :refer :all]
             [impractical-sort.bogosort :refer :all]))
 
-(deftest ascending-test
-  (testing "ascending? should"
-    (testing "return true if sorted in ascending order"
-      (is (true? (ascending? [0 1 2])))         ; common case
-      (is (true? (ascending? [1])))             ; just one
-      (is (true? (ascending? [7 7 7])))         ; all the same
-      (is (true? (ascending? [0 1 1 2 2 3]))))  ; multiple duplicate values
-    (testing "otherwise return false"
-      (is (false? (ascending? [1 0])))          ; reversed simply
-      (is (false? (ascending? [0 1 1 3 2])))))) ; dupicate and reversed
+(deftest ascending?-test
+  (testing "Returns true if sorted in ascending order"
+    (is (true? (ascending? [0 1 2])))         ; common case
+    (is (true? (ascending? [1])))             ; just one
+    (is (true? (ascending? [7 7 7])))         ; all the same
+    (is (true? (ascending? [0 1 1 2 2 3]))))  ; multiple duplicate values
+  (testing "Otherwise return false"
+    (is (false? (ascending? [1 0])))          ; reversed simply
+    (is (false? (ascending? [0 1 1 3 2]))))) ; dupicate and reversed
+
+(deftest descending?-test
+  (testing "Returns true if sorted in descending order"
+    (is (true? (descending? [2 1 0])))         ; common case
+    (is (true? (descending? [1])))             ; just one
+    (is (true? (descending? [7 7 7])))         ; all the same
+    (is (true? (descending? [3 2 2 1 1 0]))))  ; multiple duplicate values
+  (testing "Otherwise return false"
+    (is (false? (descending? [0 1])))          ; reversed simply
+    (is (false? (descending? [2 3 1 1 0])))))
 
 (deftest bogosort-test
   (let [coll [9 6 0 7 1 5 2 3 8 4]]
