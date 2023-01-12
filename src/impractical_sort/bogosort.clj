4fb6e8a1e578044631e95abd67cdbbee2e2d6f70
hisaitami <hisaitami@gmail.com>
2023-01-12 14:04:53 +0900

    Improve ascending? and descending?

--- src/impractical_sort/bogosort.clj
+++ src/impractical_sort/bogosort.clj
@@ -1,14 +1,10 @@
 (ns impractical-sort.bogosort)
 
 (defn ascending? [coll]
-  (->> (partition 2 1 coll)
-       (map #(>= 0 (apply compare %)))
-       (every? true?)))
+  (every? #(>= 0 (apply compare %)) (partition 2 1 coll)))
 
 (defn descending? [coll]
-  (->> (partition 2 1 coll)
-       (map #(<= 0 (apply compare %)))
-       (every? true?)))
+  (every? #(<= 0 (apply compare %)) (partition 2 1 coll)))
 
 (defn bogosort
   "Returns a sorted sequence of the items in coll.
@@ -20,5 +16,6 @@
        (recur pred (shuffle coll)))))
 
 ;;(time (bogosort [9 6 0 7 1 5 2 3 8 4]))
+
 ;;"Elapsed time: 6937.974985 msecs"
 ;;[0 1 2 3 4 5 6 7 8 9]
