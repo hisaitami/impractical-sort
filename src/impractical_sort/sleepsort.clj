1bfbd05b394b73d8cd1a88150c7f91d3a0a01c23
hisaitami <hisaitami@gmail.com>
2023-01-26 18:10:25 +0900

    Use (comment) for easy inline-eval

--- src/impractical_sort/sleepsort.clj
+++ src/impractical_sort/sleepsort.clj
@@ -14,7 +14,8 @@
     (doall (pmap f coll))
     @r))
 
-;;(time (sleepsort [9 6 0 7 1 5 2 3 8 4]))
+(comment
+  (time (sleepsort [9 6 0 7 1 5 2 3 8 4])))
 
 ;; "Elapsed time: 96.301592 msecs"
 ;; [0 1 2 3 4 5 6 7 8 9]
