74c5a1ae967652ee8e80f862108fc9408f2ffcab
hisaitami <hisaitami@gmail.com>
2024-08-21 08:30:36 +0900

    Upgrade clojure version to 1.11.4

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/hisaitami/impractical-sort"
   :license {:name "The MIT License"
             :url "https://github.com/hisaitami/impractical-sort/blob/main/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.11.3"]]
+  :dependencies [[org.clojure/clojure "1.11.4"]]
   :repl-options {:init-ns impractical-sort.core})
